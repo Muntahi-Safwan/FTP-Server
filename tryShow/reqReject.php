8e14a25969afb9a3a3524932ba4c78ede31ff204
Tamzzzzid <tamzid0172@gmail.com>
2026-05-18 13:50:07 +0600

    t

--- tryShow/reqReject.php
+++ tryShow/reqReject.php
@@ -1,5 +1,20 @@
 <?php
- $id=$_GET['id'];
- echo $id;
+function getConnect() {
+    return mysqli_connect('localhost', 'root', '', 'ftp_server');
+}
 
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $conn = getConnect();
+    
+    // Update the status to 'rejected'
+    $sql = "UPDATE content_requests SET status = 'rejected' WHERE id = '$id'";
+    mysqli_query($conn, $sql);
+    
+    mysqli_close($conn);
+}
+
+// Redirect back to the HTML file
+header("Location: reqShow.html");
+exit(); // Always use exit after a header redirect
 ?>
